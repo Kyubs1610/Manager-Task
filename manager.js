24773f720d6a10a1d161c104dfb1f55847b2d097
Kyubs1610 <diego.decort@gmail.com>
2022-10-13 17:44:40 +0200

    last update for today

--- manager.js
+++ manager.js
@@ -4,40 +4,70 @@
 const fs = require('fs');
 const readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
+const prompt = require(`prompt-sync`)();
 
-console.log("Welcome to your task manager, Press: ")
-let tasks = ["1. to see all your tasks", "2. to add a task", "3. to delete a task", "4. to mark a task as done", "5. to Exit the task"]
 
-let taches = ["playing video game", "listenning to music", "pet the dogs", "trying to understand JS"]
 
-const showtasks = () => {
+let menu = ` 
+Hello! Welcome to the task Manager.
+Enter a number from 1 to 5:
+1. to see all your tasks
+2. to add a task
+3. to delete a task
+4. to mark a task as done 
+5. to Exit the task manager
+`
+
+
+
+
+// task //
+
+let tasks = ["playing video game", "listening to music", "pet the dogs", "trying to understand JS"]
+
+// add task//
+
+const addTask = () => {
+    newTask = prompt("what's the new task?");
+    tasks.push(newTask); 
     console.log(tasks)
 }
 
-showtasks()
+// remove task //
+const rmvTask = () => {
+    whichTask = prompt("Which task would you like to remove?")
+    let i = whichTask;
+    splicedTask = tasks.splice(i, 1);
+}
 
+// const showtasks = () => {
+//     console.log(menu)
+// }
 
-rl.question('Enter a number from 1 to 5 ', (task) => {
-    if(task ==1 ){
-        console.log(taches)
-        rl.close();
+// showtasks() *I M WORKING ON THIS PART FOR THE MOMENT SEE YOU TOMORROW JS *
+
+rl.question('Enter a number from 1 to 5 ', (menu) => {
+    if(menu ==1 ){
+        console.log(tasks)
+        return showtasks();
     }
-    if(task ==2 ){
-        console.log(taches + "")
-        rl.close();
+    if(menu ==2 ){
+        console.log(tasks)
+        addTask();
     }
-    if(task ==3 ){
-        console.log("nikl")
-        rl.close();
+    if(menu ==3 ){
+        console.log(tasks)
+        rmvTask();
     }
-    if(task ==4 ){
+    if(menu ==4 ){
         console.log(taches + " done")
         rl.close();
     }
-    if(task ==5 ){
+    if(menu ==5 ){
+        console.log("Ciao Baby")
         rl.close();
     }
-    if(task > 5 || task <1){
+    if(menu > 5 || menu <1){
         console.log("Ha Ha Ha, vous n'avez pas dit le nombre magique")
         rl.close();
     }
