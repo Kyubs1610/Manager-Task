5defb9f7db95a8a4de5b682e12175c45b58435f9
Kyubs1610 <diego.decort@gmail.com>
2022-10-14 15:51:47 +0200

    last update for today

--- manager.js
+++ manager.js
@@ -1,34 +1,30 @@
-// MY TASK MANAGER - TERMINAL
-
-// SETUP CODE
+// SETUP CODE //
 const fs = require('fs');
+const { resolve } = require('path');
 const readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
 const prompt = require(`prompt-sync`)();
 
+// END OF THE SETUP //
 
+let tasks = [ "playing video game", "listening to music", "pet the dogs", "trying to understand JS"]
 
-let menu = ` 
-Hello! Welcome to the task Manager.
-Enter a number from 1 to 5:
+const menu = 
+    console.log (`Hello! Welcome to the task Manager:
 1. to see all your tasks
 2. to add a task
 3. to delete a task
 4. to mark a task as done 
-5. to Exit the task manager
-`
+5. to Exit the task manager`)
 
-
-
-
-// task //
-
-let tasks = ["playing video game", "listening to music", "pet the dogs", "trying to understand JS"]
+const showtasks = () => {
+    console.log(tasks)
+}
 
 // add task//
 
 const addTask = () => {
-    newTask = prompt("what's the new task?");
+    newTask = prompt("What's the new task?");
     tasks.push(newTask); 
     console.log(tasks)
 }
@@ -39,28 +35,33 @@ const rmvTask = () => {
     let i = whichTask;
     splicedTask = tasks.splice(i, 1);
 }
+//task done //
 
-// const showtasks = () => {
-//     console.log(menu)
-// }
+const doneTask = () => {
+    taskDone = prompt(`Which task is done ?`)
+    let i = taskDone;
+    tasks[i] = tasks[i] + " [V]"
+    console.log(tasks)
+}
 
-// showtasks() *I M WORKING ON THIS PART FOR THE MOMENT SEE YOU TOMORROW JS *
-
-rl.question('Enter a number from 1 to 5 ', (menu) => {
-    if(menu ==1 ){
-        console.log(tasks)
-        return showtasks();
-    }
+rl.question('Enter a number between 1 to 5 ', (menu) => {
+    if(menu == 1 ){
+        showtasks();
+        rl.close();
+         } 
     if(menu ==2 ){
         console.log(tasks)
         addTask();
-    }
+        rl.close();
+        }
     if(menu ==3 ){
         console.log(tasks)
         rmvTask();
-    }
+        rl.close();
+        }
     if(menu ==4 ){
-        console.log(taches + " done")
+        console.log(tasks)
+        doneTask();
         rl.close();
     }
     if(menu ==5 ){
@@ -68,9 +69,9 @@ rl.question('Enter a number from 1 to 5 ', (menu) => {
         rl.close();
     }
     if(menu > 5 || menu <1){
-        console.log("Ha Ha Ha, vous n'avez pas dit le nombre magique")
+        console.log("You didn't say the magic number!")
         rl.close();
-    }
-});
+}})
+;
 
-
+// I didn't find a way to return to the rl.question for the moment so I left the "rl.close" but if you have an idea let me know plz // 
